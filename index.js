2260bc458315b2ac30115bce277498deef2fd956
mtwtkman <punipuniomochi@gmail.com>
2021-04-14 19:37:50 +0900

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+const originalTitle = document.title;
+
+function extractDetail(node) {
+    const detail = node.querySelector("div#details.style-scope.ytd-macro-markers-list-item-renderer").children;
+    return {
+      name: detail[0].textContent,
+      startAt: detail[1].textContent,
+    };
+}
+
+const chapterItermsRegion = () => document.querySelector("div#contents.style-scope.ytd-macro-markers-list-renderer").children;
+const chapterItems = () => Array.from(chapterItermsRegion());
+
+function getCurrentChapter() {
+  let iterable = chapterItems();
+  // Passeiing time will never be updated unless the cursor hovers on the youtube player.
+  // So I must check it everytime.
+  let currentChapter = iterable.filter(x => x.getAttribute('active') !== null)[0];
+  return currentChapter;
+}
+
+const currentChapterTitle = currentChapter => `${originalTitle}[♪ ${extractDetail(currentChapter).name}]`;
+
+function overwriteTitle() {
+  const currentChapter = getCurrentChapter();
+  const newTitle = currentChapterTitle(currentChapter);
+  document.title = newTitle;
+};
+
+const ms = 1;
+const sec = 1000 * ms;
+
+function main() {
+  setInterval(overwriteTitle, 1 * sec);
+}
+
+main();
